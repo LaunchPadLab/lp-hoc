1014d503d6137b1cdbf5f34789daa44d623ea78b
Conor Hawes <conor@launchpadlab.com>
2018-12-21 09:12:21 -0600

    Add non transpiled version

--- src/utils/route-utils.js
+++ src/utils/route-utils.js
@@ -1,48 +1,34 @@
 // Extract from React Router v3 to enable backwards compatability for adaptToReactRouter
 // The original location of this file was node_modules/react-router/lib/RouteUtils
 
-'use strict';
-
-exports.__esModule = true;
-
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-
-exports.isReactChildren = isReactChildren;
-exports.createRouteFromReactElement = createRouteFromReactElement;
-exports.createRoutesFromReactChildren = createRoutesFromReactChildren;
-exports.createRoutes = createRoutes;
-
-var _react = require('react');
-
-var _react2 = _interopRequireDefault(_react);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+import React from 'react'
 
 function isValidChild(object) {
-  return object == null || _react2.default.isValidElement(object);
+  return object == null || React.isValidElement(object)
 }
 
-function isReactChildren(object) {
-  return isValidChild(object) || Array.isArray(object) && object.every(isValidChild);
+export function isReactChildren(object) {
+  return isValidChild(object) || (Array.isArray(object) && object.every(isValidChild))
 }
 
 function createRoute(defaultProps, props) {
-  return _extends({}, defaultProps, props);
+  return { ...defaultProps, ...props }
 }
 
-function createRouteFromReactElement(element) {
-  var type = element.type;
-  var route = createRoute(type.defaultProps, element.props);
+export function createRouteFromReactElement(element) {
+  const type = element.type
+  const route = createRoute(type.defaultProps, element.props)
 
   if (route.children) {
-    var childRoutes = createRoutesFromReactChildren(route.children, route);
+    const childRoutes = createRoutesFromReactChildren(route.children, route)
 
-    if (childRoutes.length) route.childRoutes = childRoutes;
+    if (childRoutes.length)
+      route.childRoutes = childRoutes
 
-    delete route.children;
+    delete route.children
   }
 
-  return route;
+  return route
 }
 
 /*
@@ -62,35 +48,36 @@ function createRouteFromReactElement(element) {
  * Note: This method is automatically used when you provide <Route> children
  * to a <Router> component.
  */
-function createRoutesFromReactChildren(children, parentRoute) {
-  var routes = [];
+export function createRoutesFromReactChildren(children, parentRoute) {
+  const routes = []
 
-  _react2.default.Children.forEach(children, function (element) {
-    if (_react2.default.isValidElement(element)) {
+  React.Children.forEach(children, function (element) {
+    if (React.isValidElement(element)) {
       // Component classes may have a static create* method.
       if (element.type.createRouteFromReactElement) {
-        var route = element.type.createRouteFromReactElement(element, parentRoute);
+        const route = element.type.createRouteFromReactElement(element, parentRoute)
 
-        if (route) routes.push(route);
+        if (route)
+          routes.push(route)
       } else {
-        routes.push(createRouteFromReactElement(element));
+        routes.push(createRouteFromReactElement(element))
       }
     }
-  });
+  })
 
-  return routes;
+  return routes
 }
 
 /*
  * Creates and returns an array of routes from the given object which
  * may be a JSX route, a plain object route, or an array of either.
  */
-function createRoutes(routes) {
+export function createRoutes(routes) {
   if (isReactChildren(routes)) {
-    routes = createRoutesFromReactChildren(routes);
+    routes = createRoutesFromReactChildren(routes)
   } else if (routes && !Array.isArray(routes)) {
-    routes = [routes];
+    routes = [ routes ]
   }
 
-  return routes;
+  return routes
 }
